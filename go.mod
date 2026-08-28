7654827facd4832172cb10d658ec9b4afefa1082
SerhioRS <emdetey21@yandex.ru>
2026-08-28 03:30:13 +0300

    fix sql err nill string duplicate | add validate rules | create GetUser service/repo methods

--- go.mod
+++ go.mod
@@ -12,16 +12,21 @@ require (
 
 require (
 	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator v9.31.0+incompatible // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/kr/text v0.2.0 // indirect
+	github.com/leodido/go-urn v1.5.0 // indirect
 	github.com/rogpeppe/go-internal v1.16.0 // indirect
-	golang.org/x/net v0.55.0 // indirect
-	golang.org/x/sync v0.20.0 // indirect
-	golang.org/x/sys v0.45.0 // indirect
-	golang.org/x/text v0.37.0 // indirect
+	golang.org/x/crypto v0.55.0 // indirect
+	golang.org/x/net v0.57.0 // indirect
+	golang.org/x/sync v0.22.0 // indirect
+	golang.org/x/sys v0.47.0 // indirect
+	golang.org/x/text v0.41.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20260526163538-3dc84a4a5aaa // indirect
 	google.golang.org/protobuf v1.36.12 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- internal/service/user.go
+++ internal/service/user.go
@@ -3,7 +3,8 @@ package service
 import (
 	"context"
 	"core/internal/repository"
-	"errors"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 type UserCreateParams struct {
@@ -28,11 +29,33 @@ func NewUserService(repo repository.UserRepository) UserSercive {
 	return &userService{repo: repo}
 }
 
+func (s *userService) hashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(bytes), err
+}
+
+func (s *userService) checkPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
 func (s *userService) CreateUser(ctx context.Context, params repository.UserCreateParams) (*repository.User, error) {
-	//TODO вынести в отдельный валидатор
-	if params.Email == "" || params.Password == "" {
-		return nil, errors.New("email is password are required fields")
+
+	passwordHash, err := s.hashPassword(params.Password)
+	if err != nil {
+		return nil, err
 	}
 
+	params.Password = passwordHash
+
 	return s.repo.Create(ctx, params)
+}
+
+func (s *userService) GetUser(ctx context.Context, userId int) (*repository.User, error) {
+	user, err := s.repo.GetUser(ctx, userId)
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
 }
